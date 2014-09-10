711cfef2c58e646e3ef67c4acf8b0f3cdd9d9b68
Jerome Chapron <jchapron@dotgee.fr>
2014-09-10 19:21:20 +0200

    Hotfix serialization_scope for production env

--- api/app/controllers/geocms/api/v1/base_controller.rb
+++ api/app/controllers/geocms/api/v1/base_controller.rb
@@ -2,6 +2,7 @@ module Geocms
   module Api
     module V1
       class BaseController < ActionController::Base
+        include ActionController::Serialization
         respond_to :json
         # Disable for all serializers (except ArraySerializer)
         # ActiveModel::Serializer.root = true
