527594a60d48f085516e217d0a641679d463b232
Jerome Chapron <jchapron@dotgee.fr>
2014-11-20 10:48:15 +0100

    Bugfix : Context edition in BO was broken
    Nested routes were not correctly reflected in controller.
    Changed layout to standard table.
    
    Fixes #3314

--- backend/app/controllers/geocms/backend/contexts_controller.rb
+++ backend/app/controllers/geocms/backend/contexts_controller.rb
@@ -12,51 +12,51 @@ module Geocms
       end
 
       def index
-        @folders = Folder.all
+        # @folders = Folder.all
         @contexts = Context.all
       end
 
       def show
         @context = Context.find(params[:id])
-        respond_with([:backend, @context])
+        respond_with([:backend, @folder, @context])
       end
 
       def new
         @context = Context.new
-        respond_with([:backend, @context])
+        respond_with([:backend, @folder, @context])
       end
 
       def create
         @context = Context.new(context_params)
         @context.save
-        respond_with([:backend, @context])
+        respond_with([:backend, @folder, @context])
       end
 
       def edit
         @context = Context.find(params[:id])
-        respond_with [:backend, @context]
+        respond_with [:backend, @folder, @context]
       end
 
       def update
         @context = Context.find(params[:id])
         @context.update_attributes(context_params)
-        respond_with([:edit, :backend, @context])
+        respond_with([:edit, :backend, @folder, @context])
       end
 
       def destroy
         @context = Context.find(params[:id])
         @context.destroy
-        respond_with [:backend, @context]
+        respond_with [:backend, @folder]
       end
 
       private
-        def context_params
-          params.require(:context).permit(PermittedAttributes.context_attributes)
-        end
+      def context_params
+        params.require(:context).permit(PermittedAttributes.context_attributes)
+      end
 
-        def load_folder
-          @folder = Folder.find(params[:folder_id])
-        end
+      def load_folder
+        @folder = Folder.find(params[:folder_id])
+      end
 
     end
   end
