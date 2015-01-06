6afc92acaa44fe046dee0e44e4d6d73a8e638270
Jerome Chapron <jchapron@dotgee.fr>
2015-01-06 11:08:05 +0100

    Add wrapper for inline checkbox

--- core/lib/geocms/simple_form_bootstrap.rb
+++ core/lib/geocms/simple_form_bootstrap.rb
@@ -123,6 +123,15 @@ SimpleForm.setup do |config|
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
+  config.wrappers :inline_checkbox, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+    b.use :html5
+    b.wrapper tag: 'div', class: 'controls' do |ba|
+      ba.use :label_input, wrap_with: { class: 'checkbox inline' }
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
   # Wrappers for forms and inputs using the Bootstrap toolkit.
   # Check the Bootstrap docs (http://getbootstrap.com)
   # to learn about the different styles for forms and inputs,
