5f4b78263c52516b6b95170c7133253e3b03cab3
Jerome Chapron <jchapron@dotgee.fr>
2015-02-13 10:21:27 +0100

    Load decorators to allow apps to override models
    For example, override pg_search to use french dictionnary should go
    into a decorator :
    ```
    Geocms::Layer.class_eval do
      pg_search_scope :search, against: [:name, :title],
                      :using => {
                        :tsearch => {:dictionary => "french"}
                      },
                      :ignoring => :accents
    end
    ```
    
    Keep geocms as generic as possible

--- core/lib/geocms/core/engine.rb
+++ core/lib/geocms/core/engine.rb
@@ -2,6 +2,12 @@ module Geocms
   module Core
     class Engine < ::Rails::Engine
       isolate_namespace Geocms
+
+      config.to_prepare do
+        Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
     end
   end
 end
