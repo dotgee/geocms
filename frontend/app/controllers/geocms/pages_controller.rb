e9cb359486273a46b7799592eb2c7fee0db334ad
Alban Merino <amerino@dotgee.fr>
2015-03-23 17:26:24 +0100

    [BUG] allow iframe pages

--- frontend/app/controllers/geocms/pages_controller.rb
+++ frontend/app/controllers/geocms/pages_controller.rb
@@ -2,6 +2,12 @@ require_dependency "geocms/application_controller"
 
 module Geocms
   class PagesController < ApplicationController
+    after_action :allow_iframe
+
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
+    end
+
     def index
     end
   end
