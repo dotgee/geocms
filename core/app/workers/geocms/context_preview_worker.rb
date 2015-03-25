21df04d5936591e3b04c7fc67884844b0df77226
Alban Merino <amerino@dotgee.fr>
2015-03-25 09:53:30 +0100

    Add prefix to screenshot service

--- core/app/workers/geocms/context_preview_worker.rb
+++ core/app/workers/geocms/context_preview_worker.rb
@@ -5,7 +5,9 @@ module Geocms
     def perform(context_id, current_tenant_id)
       current_tenant = Geocms::Account.find current_tenant_id
       context = Geocms::Context.find context_id
-      context.remote_preview_url = "#{current_tenant.screenshot_url.value}?url=#{current_tenant.host.value}#{context.share_url}?plugins&delay=15000"
+      remote_preview_url = "#{current_tenant.screenshot_url.value}?url=#{current_tenant.host.value}#{current_tenant.prefix_uri.value}#{context.share_url}?plugins&delay=15000"
+      logger.info "remote url : #{remote_preview_url}"
+      context.remote_preview_url = remote_preview_url
       context.save!
     end
 
