02cd6bfb5ddee314793b8f23a7776eed3389cc49
mrouan <mathias.rouan@univ-brest.fr>
2018-10-25 14:15:42 +0200

    ruby-ogc github repository url update

--- common_geocms_dependencies.rb
+++ common_geocms_dependencies.rb
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem "pg", ">= 0.14.0"
 
-gem 'ruby-ogc', :git => 'https://github.com/dotgee/ruby-ogc.git'
+gem 'ruby-ogc', :git => 'https://github.com/LETG/ruby-ogc.git'
 gem 'responders'
 
 #gem 'sass-rails',   '~> 4.0.2'
